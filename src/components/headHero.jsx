761b10a2478db4930a2ce40ea3dec2c87e85ef73
xarrijorge <kodinglife@gmail.com>
2019-03-11 02:36:36 +0000

    pretty up the headHero component. Final changes for tonight

--- src/components/headHero.jsx
+++ src/components/headHero.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 
 export default () =>(
   <div className="base head-hero">
-      <div className="content">
-        <h1 className="hero-title">This is Xarri Jorge</h1>
-        <p className="hero-sub"> Frontend developer</p>
-        <p className="hero-desc">I'm creating noice web experiences for the next generation of consumer-facing companies.</p>
+    <div className="content">
+      <div className="hero-title">
+        <h1>This is Xarri Jorge</h1>
+        <span className="hero-div">|</span>
+        <span className="hero-sub">Frontend<br/> Developer</span>
       </div>
+      <p className="hero-desc">I'm creating noice web experiences for the next generation of consumer-facing companies.</p>
     </div>
+  </div>
 )
