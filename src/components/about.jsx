013ec29961b61b93d441c6e537e00786c53ea940
Xarri Jorge <kodinglife@gmail.com>
2020-04-28 13:45:58 +0000

    Styled scrollbar. Added left border to image

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import image from '../images/xarriImage.jpg';
+import React from "react"
+import image from "../images/xarriImage.jpg"
 
-export default ()=>(
+export default () => (
   <div className="base about">
-      <div className="content">
-        <section></section>
-          <p>I'm a Frontend UI/UX designer. I love what I do and I especially enjoy the process of taking a project from concept to completion. Because of the love of the process, I've have experience in backend technologies like Node and Rails  and the stacks that come with them.</p> 
-      </div>
-      <div className="graphic">
-        <img src={image} alt="Xarri standing"/>
-      </div>
+    <div className="content">
+      <section></section>
+      <p>
+        I'm a Frontend UI/UX developer living in Monrovia, Liberia. I love what
+        I do and I especially enjoy the process of taking a design project and
+        coding it from concept to completion. I'm a Geek, Nerd and everything in
+        between.
+      </p>
     </div>
+    <div className="graphic">
+      <img src={image} alt="Xarri standing" />
+    </div>
+  </div>
 )
