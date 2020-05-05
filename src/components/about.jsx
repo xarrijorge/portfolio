425970f22b197b6b01bd3fdb757f34b0acfabe01
Xarri Jorge <kodinglife@gmail.com>
2020-05-05 23:00:51 +0000

    Complete mobile view for about component.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,12 +5,13 @@ export default () => (
   <div className="base about">
     <div className="content">
       <section></section>
-      <p>
+      <div className="textContent">
+        <img src={image} alt="Xarri image" className="mobileImage"/>
         I'm a Frontend UI/UX developer living in Monrovia, Liberia. I love what
         I do and I especially enjoy the process of taking a design project and
         coding it from concept to completion. I'm a Geek, Nerd and everything in
         between.
-      </p>
+      </div>
     </div>
     <div className="graphic">
       <img src={image} alt="Xarri standing" />
