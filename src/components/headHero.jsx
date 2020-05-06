0a3c5a7d5f93dea3417fe5d4812bcf6ee02db818
Xarri Jorge <kodinglife@gmail.com>
2020-05-06 04:45:44 +0000

    Completed hero mobile view

--- src/components/headHero.jsx
+++ src/components/headHero.jsx
@@ -3,16 +3,12 @@ import React from 'react';
 export default () =>(
   <>
   <div className="base head-hero">
-    <div className="content">
       <div className="hero-title">
         <h1>Xarri Jorge</h1>
-        <span className="hero-div1"></span>
-        <span className="hero-div2"></span>
-        <span className="hero-sub">Frontend<br/> Developer</span>
+        <p className="hero-sub">Frontend<br/> Developer</p>
       </div>
       <p className="hero-desc">Everyone deserves Beauty and Innovation<br/> 
       ... I'm delivering just that! </p>
-    </div>
   </div>
   </>
 )
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -6,8 +6,10 @@ import logo from '../images/xarrilogocropped.png';
 export default (props) =>(
   <div className="base navigation">
     <img src={logo}alt="logo image" className="logo"/>
-    <Link activeClass="active" to="about" smooth={true} spy={true} duration={500}offset={-50}>About</Link>
+    <div>
+    <Link activeClass="active" to="about" smooth={true} spy={true} duration={500}>About</Link>
     <Link activeClass="active" to="projects" smooth={true} spy={true} duration={500}>Work</Link>
+    </div>
     {/* <Link activeClass="active" to="about" smooth={true} spy={true} duration={500}>About</Link> */}
   </div>
 )
