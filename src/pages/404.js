cacc69af85d08f251797a3ec44cdc963136efd6f
Xarri Jorge <kodinglife@gmail.com>
2020-05-17 05:38:13 +0000

    Styled 404 page & mobile view.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,24 @@
-import React from "react"
+import React from 'react'
 
-import { Link } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Shapes from '../components/shapes'
+import lostGif from '../images/404.gif'
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND!</h1>
-    <h2>This is not the page you're looking for!</h2>
-    <Link to="/">Back</Link>
-    <span> to more civilized space</span>
-  </Layout>
+    <div className="missing">
+        <SEO title="404: Not found" />
+        <Shapes />
+        <div className="contentArea">
+            <p>NOT FOUND!</p>
+            <p>This is not the page you are looking for</p>
+            <Link to="/" className="neo-white">
+                <i className="fas fa-undo" /> <span>Go back</span>
+            </Link>
+            <p>to a more civilized space</p>
+        </div>
+    </div>
 )
 
 export default NotFoundPage
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,21 +12,21 @@ export default () => (
                     rel="noopener noreferrer"
                     target="_blank"
                 >
-                    <i className="fa fa-github"></i>
+                    <i className="fa fa-github" />
                 </a>
                 <a
                     href="https://twitter.com/xarrijorge"
                     rel="noopener noreferrer"
                     target="_blank"
                 >
-                    <i className="fa fa-twitter"></i>
+                    <i className="fa fa-twitter" />
                 </a>
                 <a
                     href="https://codepen.io/algorithm"
                     rel="noopener noreferrer"
                     target="_blank"
                 >
-                    <i className="fa fa-codepen"></i>
+                    <i className="fa fa-codepen" />
                 </a>
             </div>
             <p>
