9f5184518b16296c1718bc2f53930e4914014786
Xarri Jorge <kodinglife@gmail.com>
2020-05-19 13:52:49 +0000

    Added graphic to contat page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Social from '../components/social'
+import graphic from '../images/contactGraph2.jpg'
 
 export default () => (
     <div className="contactPage">
-        <h2 className="text">
-            Shoot me a line about anything. I'm always looking to chat about
-            projects ideas, work, business, open source collaborations,
-            anything.
-        </h2>
-        <form name="contact" method="post" netlify>
+        <img src={graphic} alt="" />
+        <form name="contact" method="post" netlify data-netlify="true">
+            <h2 className="text">
+                Shoot me a line about anything. I'm always looking to chat about
+                projects ideas, work, teaching, speaking, business, open source
+                collaborations, anything.
+            </h2>
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
             <textarea cols="30" rows="10" placeholder="Message"></textarea>
