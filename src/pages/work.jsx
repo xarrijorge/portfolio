04bb47d6c982138a48e6eb23ae10c6e24a190704
Xarri Jorge <kodinglife@gmail.com>
2020-07-24 20:23:35 +0000

    Edited Manifest

---
+++ src/pages/work.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default () => (
+    <div>
+        <h5>Working some awesome projects but till those are done</h5>
+        <div className="projectDesc">
+            <p>
+                Proident ad reprehenderit reprehenderit ad in. Ut commodo non
+                commodo nulla est veniam aute Lorem. Sint in laborum laboris
+                dolor. Id eu anim culpa minim tempor. Elit tempor excepteur enim
+                ut.
+            </p>
+            <a href="" className="projectLink">
+                <img src="https://dummyimage.com/300x300.png" alt="" />
+            </a>
+        </div>
+    </div>
+)
