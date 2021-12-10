8d7098931071d8fbe28e7e71fb67a4209f98ac7c
xarrijorge <kodinglife@gmail.com>
2021-12-10 17:54:06 +0000

    Changed files to tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import image from '../images/porygonPic.png'
 import Layout from '../components/layout'
-import Social from '../components/social'
+import Social from '../components/Social'
 
-export default () => (
+const About = () => (
     <Layout>
-        <div className="base about">
-            <img src={image} alt="Xarri Smiling" className="aboutPic" />
+        <div className='base about'>
+            <img src={image} alt='Xarri Smiling' className='aboutPic' />
             <Social />
-            <h2 className="text">
+            <h2 className='text'>
                 I'm a Javascript developer living in Monrovia, Liberia. I love
                 what I do and I especially enjoy the process of taking a design
                 project and coding it from concept to completion. I'm a Geek,
                 Nerd and everything in between.{' '}
             </h2>{' '}
-            <h2 className="text">
+            <h2 className='text'>
                 I love Star Wars, Anime, Rowlin and Tolkien. I aslo enjoy
                 watcing and playing Football - go Gooners - and I enoy video
                 games as well.
@@ -22,3 +22,5 @@ export default () => (
         </div>
     </Layout>
 )
+
+export default About
