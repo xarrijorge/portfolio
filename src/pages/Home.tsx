7896fde2de9acca75d6f87683600fc20ac23b250
xarrijorge <kodinglife@gmail.com>
2022-01-09 09:51:41 +0000

    Fix home nave buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import MovingBorderButton from '../components/MovingBorderButton'
 import { Link } from 'react-router-dom'
 
 const Homepage = (): JSX.Element => {
@@ -7,23 +6,23 @@ const Homepage = (): JSX.Element => {
             <div className="description">
                 <p>Hello, my name is</p>
                 <h1 className="title">Xarri George.</h1>
-                <p className="mySubText">
+                <div className="mySubText">
                     I'm a <span className="highlight">Frontend</span> focused
                     Javascript <span className="highlight">Developer</span>
                     <p>
                         I believe everyone deserves beauty and innovation, and I
                         aim to deliver just that!
                     </p>
-                </p>
+                </div>
             </div>
             <div className="homeNav">
                 <p>Learn more</p>
                 <Link to="./about">
-                    <a className="homeLink">ABOUT</a>
+                    <span className="homeLink">About Me</span>
                 </Link>
-                <p>me and see examples of my</p>
+                <p>and see examples of</p>
                 <Link to="/work">
-                    <a className="homeLink">WORK</a>
+                    <span className="homeLink">My Work</span>
                 </Link>
             </div>
         </div>
