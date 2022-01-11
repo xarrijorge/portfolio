98d57280cbcc1e219bd7c50fe2f9d96fec81c2cc
xarrijorge <kodinglife@gmail.com>
2022-01-11 02:15:55 +0000

    About page first draft

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,42 +1,38 @@
-import Layout from '../components/layout'
-import Social from '../components/Social'
-
+import image from '../images/xarri.jpg'
 const About = () => (
-    <Layout>
-        <div className="base about">
-            <Social />
-            <h2 className="text">
-                I'm a Javascript developer living in Monrovia, Liberia. I love
-                what I do and I especially enjoy the process of taking a design
-                project and coding it from concept to completion. I'm a Geek,
-                Nerd and everything in between.{' '}
-            </h2>{' '}
-            <h2 className="text">
-                I love Star Wars, Anime, Rowlin and Tolkien. I aslo enjoy
-                watcing and playing Football - go Gooners - and I enoy video
-                games as well.
-            </h2>
-            <h2>
-                Hello, I'm Xarri. I'm a Javascript developer living in Monrovia,
-                Liberia, specializing in React and Node. I'm a frontend
-                developer and UI/UX designer. I'm passionate about building
-                beautiful, functional and accessible user interfaces. I have
-                experience in building websites and web applications. I'm
-                currently looking for a position in the web development
-                industry. I also have experience in Network infrastructure and
-                agent management and technical support.
-                <h2>
-                    Hello, I'm Xarri. I'm a Javascript developer living in
-                    Monrovia, Liberia, specializing in React and Node. I'm a
-                    frontend developer and UI/UX designer. I'm passionate about
-                    building beautiful, functional and accessible user
-                    interfaces. I have experience in building websites and web
-                    applications. I'm currently looking for a position in the
-                    web development industry. I also have experience in Network
-                </h2>
-            </h2>
+    <div className="about">
+        <div className="title">
+            <h1>About Me</h1>
+            <img src={image} alt="" />
         </div>
-    </Layout>
+        <div className="details">
+            <div className="col1">
+                <h3>I'm Xarri George. A Developer, maker and problem solver</h3>
+            </div>
+            <div className="col2"></div>
+            <div className="col3">
+                <p className="bio">
+                    Frontend, UI/UX developer. I love what I do and I especially
+                    enjoy the process of taking a design project and coding it
+                    from concept to completion. I'm passionate about building
+                    beautiful, functional and accessible user interfaces.
+                </p>
+
+                <p className="hobbies">
+                    I'm a Geek, Nerd and everything in between. I love Star
+                    Wars, Anime, Rowlin and Tolkien.
+                </p>
+                {/* 
+                <p>
+                     I have experience in building websites and
+                    web applications. I'm currently looking for a position in
+                    the web development industry. I also have experience in
+                    Network infrastructure and agent management and technical
+                    support.
+                </p> */}
+            </div>
+        </div>
+    </div>
 )
 
 export default About
