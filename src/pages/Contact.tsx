5b6bcc7a13107df43e97be3c2328908675b8c628
xarrijorge <kodinglife@gmail.com>
2022-01-12 21:02:00 +0000

    Fix eslint errors

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 import graphic from '../images/contactGraph2.jpg'
 
 const Contact = () => (
-    <div className='contactPage'>
-        <img src={graphic} alt='' />
-        <form name='contact' method='post' data-netlify='true'>
-            <h2 className='text'>
+    <div className="contactPage">
+        <img src={graphic} alt="" />
+        <form name="contact" method="post" data-netlify="true">
+            <h2 className="text">
                 Shoot me a line about anything. I'm always looking to chat about
                 projects ideas, work, teaching, speaking, business, open source
                 collaborations, anything.
             </h2>
-            <input type='text' placeholder='Name' />
-            <input type='email' placeholder='Email' />
-            <textarea cols={30} rows={10} placeholder='Message'></textarea>
-            <button className='neo-white' type='submit'>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <textarea cols={30} rows={10} placeholder="Message"></textarea>
+            <button className="neo-white" type="submit">
                 Send
             </button>
         </form>
